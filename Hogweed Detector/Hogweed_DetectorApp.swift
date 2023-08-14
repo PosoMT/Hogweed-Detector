4fd67d6e0ea1a597b574c0d1bf40ec007fa20ebe
PosoMT <posobilo.p@gmail.com>
2023-08-14 15:04:14 +0300

    Initial Commit

---
+++ Hogweed Detector/Hogweed_DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hogweed_DetectorApp.swift
+//  Hogweed Detector
+//
+//  Created by Pablo on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Hogweed_DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
