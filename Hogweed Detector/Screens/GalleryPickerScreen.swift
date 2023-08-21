025cfcd9ae5f6435b800204e79852972932214b7
PosoMT <posobilo.p@gmail.com>
2023-08-21 20:20:07 +0300

    a bit more

--- Hogweed Detector/Screens/GalleryPickerScreen.swift
+++ Hogweed Detector/Screens/GalleryPickerScreen.swift
@@ -10,7 +10,7 @@ import PhotosUI
 import Vision
 
 struct GalleryPickerScreen: View {
-    @StateObject private var handler = ExampleVisionHandler()
+    @StateObject private var handler = VisionHandlerVM()
     @State private var selectedItem: PhotosPickerItem?
     @State private var selectedSourceType: UIImagePickerController.SourceType?
     @State private var showCamera = false
@@ -121,82 +121,6 @@ struct GalleryPickerScreen: View {
             }
             .navigationBarTitle("Найти на фото", displayMode: .inline)
         }
-    }
-}
-
-class ExampleVisionHandler: ObservableObject {
-    var image: CGImage!
-    
-    private var requests: [VNRequest] = []
-    @Published var results: [VNRecognizedObjectObservation] = []
-    @Published var confidence: Float = 75.0
-    let labelColorMapping: [String: Color] = [
-        "hogweed_sosnowskyi": .red,
-        "Umbelliferae": .blue,
-    ]
-    private let labelNames = ["hogweed_sosnowskyi" : "Борщевик", "Umbelliferae" : "Зонтичное"]
-    
-    init() {
-        setupVision()
-    }
-    
-    func scan(_ image: CGImage?) {
-        if let image = image {
-            self.image = image
-            performRequests()
-        }
-    }
-    
-    private func performRequests() {
-        let imageRequestHandler = VNImageRequestHandler(cgImage: self.image)
-        do {
-            try imageRequestHandler.perform(self.requests)
-        } catch {
-            print(error)
-        }
-    }
-    
-    @discardableResult
-    func setupVision() -> NSError? {
-        let error: NSError! = nil
-        
-        guard let modelURL = Bundle.main.url(forResource: "Hogweed3", withExtension: "mlmodelc") else {
-            return NSError(domain: "VisionHandler", code: -1, userInfo: [NSLocalizedDescriptionKey: "Model file is missing"])
-        }
-        do {
-            let visionModel = try VNCoreMLModel(for: MLModel(contentsOf: modelURL))
-            let objectRecognition = VNCoreMLRequest(model: visionModel, completionHandler: { (request, error) in
-                guard let results = request.results as? [VNRecognizedObjectObservation] else { return }
-                guard results.isEmpty == false else {
-                    self.results.removeAll()
-                    print("Results array is empty, was handling \(self.requests.count) requests"); return
-                    
-                }
-                
-                let filtered = results.filter { $0.labels.first?.confidence ?? 0 >= self.confidence / 100 }
-                self.results = filtered
-            })
-            objectRecognition.imageCropAndScaleOption = .scaleFit
-            self.requests = [objectRecognition]
-        } catch let error as NSError {
-            print("Model loading went wrong: \(error)")
-        }
-        
-        return error
-    }
-    
-    func clearResults() {
-        results.removeAll()
-    }
-    
-    // MARK: ui processing
-    
-    func getProperLabelName(label: String) -> String {
-        return labelNames[label] ?? "Неизвестное значение"
-    }
-    
-    func deNormalize(_ rect: CGRect, _ geometry: GeometryProxy) -> CGRect {
-        return VNImageRectForNormalizedRect(rect, Int(geometry.size.width), Int(geometry.size.height))
     }
 }
 
